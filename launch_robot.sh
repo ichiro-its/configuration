e8f8da031091e2ce1f14acf26adf1d43ae3d6f47
finesaaa <indriatifiqey16@gmail.com>
2021-06-11 20:25:48 +0700

    reafctor: update sh

--- launch_robot.sh
+++ launch_robot.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
-
-cd /home/ichiro/ros2s-ws
+#!/bin/bash
 
 ROBOT_ID=${ROBOCUP_ROBOT_ID:=1}
 
@@ -10,9 +8,9 @@ ADDR=${ROBOCUP_SIMULATOR_ADDR:=127.0.0.1:10001}
 IP=$(cut -d: -f1 <(echo $ADDR))
 PORT=$(cut -d: -f2 <(echo $ADDR))
 
-PATH=${ICONFIG_PATH:="/home/ichiro/ros2-ws/configuration/walking/"}
-PKG=${IPKG:="aruku"}
-EXC=${IEXC:="walking_main"}
+# PATH=${ICONFIG_PATH:="/home/ichiro/ros2-ws/configuration/walking/"}
+# PKG=${IPKG:="aruku"}
+# EXC=${IEXC:="walking_main"}
 echo "Robot: $ROBOT_ID"
 
 source /opt/ros/foxy/setup.sh
@@ -21,7 +19,7 @@ source /home/ichiro/ros2-ws/install/local_setup.sh
 echo "Launching robot $ROBOT_ID in IP $IP and port $PORT, please wait!"
 
 while true; do
-    ros2 run $PKG $EXC $IP $PORT $PATH 
+    ros2 run aruku walking_main $IP $PORT /home/ichiro/ros2-ws/configuration/walking/ 
     sleep 1
 done
 
