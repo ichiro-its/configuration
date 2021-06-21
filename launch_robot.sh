b1e628949d79f560f93f66ba5deae548cfc79256
finesaaa <indriatifiqey16@gmail.com>
2021-06-21 14:51:31 +0700

    fix: export USER and env

--- launch_robot.sh
+++ launch_robot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ROBOT_ID=${ROBOCUP_ROBOT_ID:=1}
 
@@ -8,16 +8,16 @@ ADDR=${ROBOCUP_SIMULATOR_ADDR:=127.0.0.1:10001}
 IP=$(cut -d: -f1 <(echo $ADDR))
 PORT=$(cut -d: -f2 <(echo $ADDR))
 
-PATH=${ICONFIG_PATH:="/home/ichiro/configuration/"}
+export USER=ichiro
 echo "Playing Robot $ROBOT_ID"
 
 source /opt/ros/foxy/setup.sh
-source /home/ichiro/soccer/install/local_setup.sh
+source /home/ichiro/soccer/install/setup.sh
 
 echo "Launching robot $ROBOT_ID in IP $IP and port $PORT, please wait!"
 
 while true; do
-    ros2 run soccer robocup_soccer_run $IP $PORT $PATH $ROBOT_ID 
+    ros2 run soccer robocup_soccer_run $IP $PORT /home/ichiro/configuration/ $ROBOT_ID 
     sleep 1
 done
 
