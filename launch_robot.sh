ea52b41070a12e9f5353608bd143d3b2726c7da8
finesaaa <indriatifiqey16@gmail.com>
2021-06-25 21:48:40 +0700

    feat: new

--- launch_robot.sh
+++ launch_robot.sh
@@ -19,7 +19,7 @@ source /home/ichiro/ros2-ws/install/setup.sh
 echo "Launching robot $ROBOT_ID in IP $IP and port $PORT, please wait!"
 
 while true; do
-    ros2 run soccer robocup_soccer_run $IP $PORT /home/ichiro/configuration/ $ROBOT_ID $TEAMMIRRORIP
+    ros2 run soccer robocup_soccer_run $IP $PORT /home/ichiro/configuration/ $ROBOT_ID $ROBOCUP_MIRROR_SERVER_IP
     sleep 1
 done
 
