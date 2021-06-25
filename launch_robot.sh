deb2f29006dcc1824f275e7d8389d92afe846416
finesaaa <indriatifiqey16@gmail.com>
2021-06-25 21:12:25 +0700

    feat: update all

--- launch_robot.sh
+++ launch_robot.sh
@@ -7,17 +7,19 @@ TEAM_COLOR=${ROBOCUP_TEAM_COLOR:="blue"}
 ADDR=${ROBOCUP_SIMULATOR_ADDR:=127.0.0.1:10001}
 IP=$(cut -d: -f1 <(echo $ADDR))
 PORT=$(cut -d: -f2 <(echo $ADDR))
+TEAMMIRRORIP=${TEAMMIRRORIP:=127.0.0.1}
+TEAMMIRRORPORT=3737
 
 export USER=ichiro
 echo "Playing Robot $ROBOT_ID"
 
 source /opt/ros/foxy/setup.sh
-source /home/ichiro/soccer/install/setup.sh
+source /home/ichiro/ros2-ws/install/setup.sh
 
 echo "Launching robot $ROBOT_ID in IP $IP and port $PORT, please wait!"
 
 while true; do
-    ros2 run soccer robocup_soccer_run $IP $PORT /home/ichiro/configuration/ $ROBOT_ID 
+    ros2 run soccer robocup_soccer_run $IP $PORT /home/ichiro/configuration/ $ROBOT_ID $TEAMMIRRORIP
     sleep 1
 done
 
