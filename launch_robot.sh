346f99a964fc65ec1a2c01d22c5ed9a26fd3324c
finesaaa <indriatifiqey16@gmail.com>
2021-06-25 23:32:42 +0700

    update walking, fix sh

--- launch_robot.sh
+++ launch_robot.sh
@@ -7,7 +7,7 @@ TEAM_COLOR=${ROBOCUP_TEAM_COLOR:="blue"}
 ADDR=${ROBOCUP_SIMULATOR_ADDR:=127.0.0.1:10001}
 IP=$(cut -d: -f1 <(echo $ADDR))
 PORT=$(cut -d: -f2 <(echo $ADDR))
-TEAMMIRRORIP=${ROBOCUP_MIRROR_SERVER_IP:=127.0.0.1}
+MIRROR_SERVER_IP=${ROBOCUP_MIRROR_SERVER_IP:=127.0.0.1}
 TEAMMIRRORPORT=3737
 
 export USER=ichiro
@@ -19,7 +19,7 @@ source /home/ichiro/ros2-ws/install/setup.sh
 echo "Launching robot $ROBOT_ID in IP $IP and port $PORT, please wait!"
 
 while true; do
-    ros2 run soccer robocup_soccer_run $IP $PORT /home/ichiro/configuration/ $ROBOT_ID $ROBOCUP_MIRROR_SERVER_IP
+    ros2 run soccer robocup_soccer_run $IP $PORT /home/ichiro/configuration/ $ROBOT_ID $MIRROR_SERVER_IP
     sleep 1
 done
 
